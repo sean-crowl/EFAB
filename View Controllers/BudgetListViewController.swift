a7aa01c75ffaaecac5d1db04da947637c42764be
Sean Crowl <sean.crowl@interapthq.com>
2016-11-01 16:55:45 -0400

    Added login screen and stuff

---
+++ View Controllers/BudgetListViewController.swift
@@ -0,0 +1,37 @@
+//
+//  BudgetListViewController.swift
+//  EFAB
+//
+//  Created by Sean Crowl on 11/1/16.
+//  Copyright © 2016 Interapt. All rights reserved.
+//
+
+import UIKit
+
+class BudgetListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if !WebServices.shared.userAuthTokenExists() || WebServices.shared.userAuthTokenExpired() {
+            performSegue(withIdentifier: "PresentLoginNoAnimation", sender: self)
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ View Controllers/RegisterViewController.swift
@@ -0,0 +1,43 @@
+//
+//  RegisterViewController.swift
+//  EFAB
+//
+//  Created by Sean Crowl on 11/1/16.
+//  Copyright © 2016 Interapt. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: - IBActions
+    @IBAction func registerButtonClicked(_ sender: Any) {
+    }
+    
+}
